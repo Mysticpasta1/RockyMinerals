fd2a1b1ffcdbcfcc06d9a874f54114654d1c22ef
Mysticpasta1 <ethana5tc@outlook.com>
2025-04-07 23:38:40 -0500

    Update LampVariantBlock.java

--- src/main/java/com/mystic/rockyminerals/block/LampVariantBlock.java
+++ src/main/java/com/mystic/rockyminerals/block/LampVariantBlock.java
@@ -3,13 +3,14 @@ package com.mystic.rockyminerals.block;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RedstoneLampBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 public class LampVariantBlock extends RedstoneLampBlock {
     public LampVariantBlock() {
-        super(Properties.copy(Blocks.STONE).lightLevel((blockState) -> blockState.getValue(BlockStateProperties.LIT) ? 15 : 0));
+        super(BlockBehaviour.Properties.copy(Blocks.STONE).lightLevel((blockState) -> blockState.getValue(BlockStateProperties.LIT) ? 15 : 0));
         this.registerDefaultState(this.defaultBlockState().setValue(LIT, false));
     }
 
