2193eb0059a9b9fa5efed31eab40bee60dc26725
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-04-08 02:16:09 -0600

    Main.java: Added res() & LOGGER

--- src/main/java/com/mystic/rockyminerals/Main.java
+++ src/main/java/com/mystic/rockyminerals/Main.java
@@ -2,16 +2,25 @@ package com.mystic.rockyminerals;
 
 import com.mystic.rockyminerals.datagen.MineralProviders;
 import com.mystic.rockyminerals.init.Init;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 @Mod(Main.MOD_ID)
 public class Main {
     public static final String MOD_ID = "rockyminerals";
+    public static final Logger LOGGER = LogManager.getLogger("Rocky Minerals");
 
     public Main(FMLJavaModLoadingContext context) {
         var bus = context.getModEventBus();
         Init.init(bus);
         MineralProviders.init(bus);
+    }
+
+    public static ResourceLocation res(String path) {
+        return new ResourceLocation(MOD_ID, path);
     }
 }
