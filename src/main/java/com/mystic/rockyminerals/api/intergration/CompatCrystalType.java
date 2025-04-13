654691aeec63a22a7d7ad7e88c0958be3b7db644
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-04-13 03:22:34 -0600

    MULTIPLE CLASSES: Updated

--- src/main/java/com/mystic/rockyminerals/api/intergration/CompatCrystalType.java
+++ src/main/java/com/mystic/rockyminerals/api/intergration/CompatCrystalType.java
@@ -12,14 +12,22 @@ public class CompatCrystalType {
     }
 
 //!! CrystalType
-    public static void simpleStoneFinder(String modId, String nameCrystalType) {
+
+    /**
+     * @param nameCrystalType ID of the StoneType for all of children including the crystal
+     */
+    public static void simpleCrystalFinder(String modId, String nameCrystalType) {
         var stonetypeFinder = CrystalType.Finder.simple(modId, nameCrystalType, nameCrystalType);
 
         BlockSetAPI.addBlockTypeFinder(CrystalType.class, stonetypeFinder);
     }
 
-    public static void stoneFinder(String modId, String nameCrystalType, String nameStone) {
-        var stonetypeFinder = CrystalType.Finder.simple(modId, nameCrystalType, nameStone);
+    /**
+     * @param nameCrystalType ID of the StoneType for all of children
+     * @param nameCrystal ID of the stone block - some block have unique naming like amethyst_block
+     */
+    public static void crystalBlockFinder(String modId, String nameCrystalType, String nameCrystal) {
+        var stonetypeFinder = CrystalType.Finder.simple(modId, nameCrystalType, nameCrystal);
 
         BlockSetAPI.addBlockTypeFinder(CrystalType.class, stonetypeFinder);
     }
--- src/main/java/com/mystic/rockyminerals/api/intergration/CompatStoneType.java
+++ src/main/java/com/mystic/rockyminerals/api/intergration/CompatStoneType.java
@@ -11,13 +11,21 @@ public class CompatStoneType {
 
 
 //!! StoneType
+
+    /**
+     * @param nameStoneType ID of the StoneType for all of children including the stone
+     */
     public static void simpleStoneFinder(String modId, String nameStoneType) {
         var stonetypeFinder = StoneType.Finder.simple(modId, nameStoneType, nameStoneType);
 
         BlockSetAPI.addBlockTypeFinder(StoneType.class, stonetypeFinder);
     }
 
-    public static void stoneFinder(String modId, String nameStoneType, String nameStone) {
+    /**
+     * @param nameStoneType ID of the StoneType for all of children
+     * @param nameStone ID of the stone block - some block have unique naming like amethyst_block
+     */
+    public static void stoneBlockFinder(String modId, String nameStoneType, String nameStone) {
         var stonetypeFinder = StoneType.Finder.simple(modId, nameStoneType, nameStone);
 
         BlockSetAPI.addBlockTypeFinder(StoneType.class, stonetypeFinder);
