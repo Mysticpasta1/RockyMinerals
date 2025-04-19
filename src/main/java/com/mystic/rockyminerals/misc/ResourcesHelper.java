974a82d511b873c97fbc5c2cab217227fc22ba6b
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-04-19 00:35:59 -0600

    ResourcesHelper: Added blue_calcite's textures

--- src/main/java/com/mystic/rockyminerals/misc/ResourcesHelper.java
+++ src/main/java/com/mystic/rockyminerals/misc/ResourcesHelper.java
@@ -7,38 +7,7 @@ import java.util.List;
 
 public class ResourcesHelper {
 
-/// WIP
-//    public static List<ResourceLocation> baseFileReference = List.of(
-//
-//            RockyMineral.res("stonetype_redstone_lamp"),
-
-//            RockyMineral.res("stonetype"),
-//            RockyMineral.res("stonetype_tile"),
-//            RockyMineral.res("stonetype_bricks"),
-//            RockyMineral.res("stonetype_lamp"),
-//            RockyMineral.res("stonetype_pillar"),
-//            RockyMineral.res("polished_stonetype"),
-//            RockyMineral.res("mosaic_stonetype"),
-//            RockyMineral.res("cut_stonetype"),
-//            RockyMineral.res("cracked_stonetype"),
-//            RockyMineral.res("cobbled_stonetype"),
-//            RockyMineral.res("chiseled_stonetype"),
-//
-//            RockyMineral.res("stonetype_X_button"),
-//            RockyMineral.res("stonetype_X_pressure_plate"),
-//            RockyMineral.res("stonetype_X_slab"),
-//            RockyMineral.res("stonetype_X_stairs"),
-//            RockyMineral.res("stonetype_X_wall"),
-//
-//            RockyMineral.res("X_button"),
-//            RockyMineral.res("X_pressure_plate"),
-//            RockyMineral.res("X_slab"),
-//            RockyMineral.res("X_stairs"),
-//            RockyMineral.res("X_wall")
-//
-//    );
-
-    public static List<TextureInfo> textureReference = List.of(
+    public static List<TextureInfo> saltstoneTemplates = List.of(
             TextureInfo.of("rockyminerals:cobbled_stone", RockyMineral.res("block/template/cobbled_stonetype")).build(),
             TextureInfo.of("rockyminerals:cracked_stone", RockyMineral.res("block/template/cracked_stonetype")).build(),
             TextureInfo.of("rockyminerals:cut_stone", RockyMineral.res("block/template/cut_stonetype")).build(),
@@ -54,7 +23,27 @@ public class ResourcesHelper {
             TextureInfo.of("rockyminerals:stone_pillar", RockyMineral.res("block/template/stonetype_pillar")).build(),
             TextureInfo.of("rockyminerals:stone_pillar", RockyMineral.res("block/template/stonetype_pillar_top")).build(),
 
-            TextureInfo.of("rockyminerals:stone_redstone_lamp", RockyMineral.res("block/template/stonetype_redstone_lamp_lit"), RockyMineral.res("block/template/stonetype_redstone_lamp_common_m")).build(),
-            TextureInfo.of("rockyminerals:stone_redstone_lamp", RockyMineral.res("block/template/stonetype_redstone_lamp_unlit"), RockyMineral.res("block/template/stonetype_redstone_lamp_common_m")).build()
+            TextureInfo.of("rockyminerals:stone_redstone_lamp", RockyMineral.res("block/template/stonetype_redstone_lamp_lit"), RockyMineral.res("block/mask/rocktype_redstone_lamp_common")).build(),
+            TextureInfo.of("rockyminerals:stone_redstone_lamp", RockyMineral.res("block/template/stonetype_redstone_lamp_unlit"), RockyMineral.res("block/mask/rocktype_redstone_lamp_common")).build()
+    );
+
+    public static List<TextureInfo> BlueCaciteTemplates = List.of(
+            TextureInfo.of("rockyminerals:cobbled_blue_calcite", RockyMineral.res("block/cobbled_blue_calcite")).build(),
+            TextureInfo.of("rockyminerals:cracked_blue_calcite", RockyMineral.res("block/cracked_blue_calcite")).build(),
+            TextureInfo.of("rockyminerals:cut_blue_calcite", RockyMineral.res("block/cut_blue_calcite")).build(),
+            TextureInfo.of("rockyminerals:mosaic_blue_calcite", RockyMineral.res("block/mosaic_blue_calcite")).build(),
+            TextureInfo.of("rockyminerals:polished_blue_calcite", RockyMineral.res("block/polished_blue_calcite")).build(),
+            TextureInfo.of("rockyminerals:blue_calcite_bricks", RockyMineral.res("block/blue_calcite_bricks")).build(),
+            TextureInfo.of("rockyminerals:blue_calcite_lamp", RockyMineral.res("block/blue_calcite_lamp")).build(),
+            TextureInfo.of("rockyminerals:blue_calcite_tile", RockyMineral.res("block/blue_calcite_tile")).build(),
+
+            TextureInfo.of("rockyminerals:chiseled_blue_calcite", RockyMineral.res("block/chiseled_blue_calcite")).build(),
+            TextureInfo.of("rockyminerals:chiseled_blue_calcite", RockyMineral.res("block/chiseled_blue_calcite_top")).build(),
+
+            TextureInfo.of("rockyminerals:blue_calcite_pillar", RockyMineral.res("block/blue_calcite_pillar")).build(),
+            TextureInfo.of("rockyminerals:blue_calcite_pillar", RockyMineral.res("block/blue_calcite_pillar_top")).build(),
+
+            TextureInfo.of("rockyminerals:blue_calcite_redstone_lamp", RockyMineral.res("block/blue_calcite_redstone_lamp_lit"), RockyMineral.res("block/mask/rocktype_redstone_lamp_common")).build(),
+            TextureInfo.of("rockyminerals:blue_calcite_redstone_lamp", RockyMineral.res("block/blue_calcite_redstone_lamp_unlit"), RockyMineral.res("block/mask/rocktype_redstone_lamp_common")).build()
     );
 }
