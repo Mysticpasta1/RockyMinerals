3c3228055d2f2ea1194b5637596025e21327c7e2
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-04-20 19:16:30 -0600

    ResourcesGenerator: Updated to include blockstates/models generation

--- src/main/java/com/mystic/rockyminerals/dynamicpack/ResourcesGenerator.java
+++ src/main/java/com/mystic/rockyminerals/dynamicpack/ResourcesGenerator.java
@@ -1,83 +1,113 @@
 package com.mystic.rockyminerals.dynamicpack;
 
-import com.google.common.base.Preconditions;
-import com.google.gson.JsonElement;
 import com.mystic.rockyminerals.RockyMineral;
 import com.mystic.rockyminerals.api.TextureInfo;
-import com.mystic.rockyminerals.api.set.*;
-import com.mystic.rockyminerals.misc.ResourcesUtils;
-import net.mehvahdjukaar.moonlight.api.resources.BlockTypeResTransformer;
+import com.mystic.rockyminerals.api.set.MineralType;
+import com.mystic.rockyminerals.api.set.MineralTypeRegistry;
+import com.mystic.rockyminerals.api.set.StoneType;
+import com.mystic.rockyminerals.api.set.StoneTypeRegistry;
 import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
-import net.mehvahdjukaar.moonlight.api.resources.ResType;
-import net.mehvahdjukaar.moonlight.api.resources.StaticResource;
 import net.mehvahdjukaar.moonlight.api.resources.pack.DynClientResourcesGenerator;
 import net.mehvahdjukaar.moonlight.api.resources.textures.Respriter;
 import net.mehvahdjukaar.moonlight.api.resources.textures.TextureImage;
 import net.mehvahdjukaar.moonlight.api.set.BlockType;
-import net.mehvahdjukaar.moonlight.api.util.Utils;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.*;
 import java.util.function.Supplier;
 
 import static com.mystic.rockyminerals.misc.ResourcesHelper.BlueCacite_Templates;
 import static com.mystic.rockyminerals.misc.ResourcesHelper.Saltstone_Templates;
+import static com.mystic.rockyminerals.misc.ResourcesUtils.*;
 
 public class ResourcesGenerator {
 
+    public static Map<BlockType, ArrayList<Block>> decorativeBlockTypes = new HashMap<>();
+    public static Map<BlockType, ArrayList<Item>> decorativeItemTypes = new HashMap<>();
+
     public static void generateResources(DynClientResourcesGenerator generator, ResourceManager manager) {
+
         for (StoneType currentType : StoneTypeRegistry.getTypes()) {
             if (currentType.isVanilla()) continue;
-            generateTexture(currentType, Saltstone_Templates, generator, manager);
+            BlockType baseType = StoneTypeRegistry.getSaltstoneType();
+            generateTexture("stonetype", currentType, Saltstone_Templates, generator, manager);
+
+            /// Modifying blockstates & models files - contents are included, too
+            generateStandardResources(currentType, decorativeBlockTypes.get(currentType), baseType,
+                    makeBlockStateTransformer(baseType, manager), makeModelTransformer(baseType, manager),
+                    generator, manager
+            );
+
+            /// Modifying models/item files - contents are included, too
+            if (!decorativeItemTypes.isEmpty()) {
+                generateStandardItemModels(currentType, decorativeItemTypes.get(currentType), baseType,
+                        makeModelTransformer(baseType, manager), generator, manager
+                );
+            }
 
         }
         for (MineralType currentType : MineralTypeRegistry.getTypes()) {
             if (currentType.isVanilla()) continue;
-            generateTexture(currentType, BlueCacite_Templates, generator, manager);
+            BlockType baseType = StoneTypeRegistry.getSaltstoneType();
+            generateTexture("blue_calcite", currentType, BlueCacite_Templates, generator, manager);
+
+            /// Modifying blocks' files
+            generateStandardResources(currentType, decorativeBlockTypes.get(currentType), baseType,
+                    makeBlockStateTransformer(baseType, manager), makeModelTransformer(baseType, manager),
+                    generator, manager
+            );
+
+            /// Modifying items' files
+            if (!decorativeItemTypes.isEmpty()) {
+                generateStandardItemModels(currentType, decorativeItemTypes.get(currentType), baseType,
+                        makeModelTransformer(baseType, manager), generator, manager
+                );
+            }
         }
     }
 
-    private static void generateTexture(BlockType blockType, List<TextureInfo> textureTemplates, DynClientResourcesGenerator generator, ResourceManager manager) {
+    private static void generateTexture(String targetBlockType, BlockType blockType, List<TextureInfo> textureTemplates, DynClientResourcesGenerator generator, ResourceManager manager) {
 
-        try (TextureImage stoneTexture = TextureImage.open(manager,
-                RPUtils.findFirstBlockTextureLocation(manager, blockType.getBlockOfThis("stone")))
-        ) {
-            for (TextureInfo textureInfo : textureTemplates) {
-                /// Creating new Path to add the new textures to the resources
-                String newResLoc = textureInfo.texture().toString()
-                        .replace("template/", "")
-                        .replace("stonetype", blockType.getTypeName());
+        if (!targetBlockType.equals(blockType.getTypeName())) { // Skip the blocktype's template
+            try (TextureImage stoneTexture = TextureImage.open(manager,
+                    RPUtils.findFirstBlockTextureLocation(manager, blockType.getBlockOfThis("stone")))
+            ) {
+                for (TextureInfo textureInfo : textureTemplates) {
+                    /// Creating new Path to add the new textures to the resources
+                    String newResLoc = textureInfo.texture().toString()
+                            .replace("template/", "")
+                            .replace(targetBlockType, blockType.getTypeName());
 
-                try (TextureImage baseTexture = TextureImage.open(manager, textureInfo.texture())
-                ) {
-                    TextureImage maskTexture;
-                    Respriter respriter;
+                    try (TextureImage baseTexture = TextureImage.open(manager, textureInfo.texture())
+                    ) {
+                        TextureImage maskTexture;
+                        Respriter respriter;
 
-                    if (Objects.nonNull(textureInfo.mask())) {
-                        maskTexture = TextureImage.open(manager, textureInfo.mask());
-                        respriter = Respriter.masked(baseTexture, maskTexture);
+                        if (Objects.nonNull(textureInfo.mask())) {
+                            maskTexture = TextureImage.open(manager, textureInfo.mask());
+                            respriter = Respriter.masked(baseTexture, maskTexture);
+                        }
+                        else {
+                            respriter = Respriter.of(baseTexture);
+                        }
+
+                        /// Swapping out the old palettes of the texture with new plattes
+                        Supplier<TextureImage> finishedTexture = () -> respriter.recolorWithAnimationOf(stoneTexture);
+
+                        /// Adding the textures to the resource
+                        generator.addTextureIfNotPresent(manager, newResLoc, finishedTexture);
                     }
-                    else {
-                        respriter = Respriter.of(baseTexture);
+                    catch (IOException e) {
+                        generator.getLogger().error("Failed to generate texture for {} with {} : {}", textureInfo.blockId(), blockType.getTypeName(), e);
                     }
-
-                    /// Swapping out the old palettes of the texture with new plattes
-                    Supplier<TextureImage> finishedTexture = () -> respriter.recolorWithAnimationOf(stoneTexture);
-
-                    /// Adding the textures to the resource
-                    generator.addTextureIfNotPresent(manager, newResLoc, finishedTexture);
                 }
-                catch (IOException e) {
-                    generator.getLogger().error("Failed to generate texture for {} with {} : {}", textureInfo.blockId(), blockType.getTypeName(), e);
-                }
+
+            } catch (Exception e) {
+                RockyMineral.LOGGER.error(e.getMessage());
             }
-
-        } catch (Exception e) {
-            RockyMineral.LOGGER.error(e.getMessage());
         }
     }
 
